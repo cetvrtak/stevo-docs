8f2671b0553370eaca0eddb6ad18f2973f7bb7e5
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2024-06-09 18:19:10 +0200

    Format document data as tables

--- src/createDocxFromSheetData.ts
+++ src/createDocxFromSheetData.ts
@@ -1,14 +1,26 @@
-import { Document, Paragraph, TextRun } from "docx";
+import { Document, Table, TableCell, TableRow, Paragraph } from "docx";
 
 export function createDocxFromSheetData(sheetDataArray: string[][][]): Document {
+    const tables = sheetDataArray
+        .filter(sheetData => sheetData.length > 0) // Filter out empty arrays
+        .map(sheetData => {
+            const rows = sheetData.map(row => new TableRow({
+                children: row.map(cell => new TableCell({
+                    children: [new Paragraph(cell)]
+                }))
+            }));
+
+            return new Table({
+                width: { size: 100, type: 'pct' },
+                rows: rows,
+            });
+        });
+
     const doc = new Document({
         sections: [
             {
                 properties: {},
-                children: sheetDataArray.flatMap(sheetData => sheetData.map(row => new Paragraph({
-                    children: [new TextRun(row.join(', '))]
-                }))
-                )
+                children: tables
             }
         ]
     });
